10f207707f22a462dd3901112a62eb060e9c448f
Rosario Cafaro <cafaro.rosario@gmail.com>
2021-09-17 19:17:11 +0200

    first commit

---
+++ .history/cs_20210917153838.cs
@@ -0,0 +1,14 @@
+using System;
+namespace HelloWorld
+{
+  class Hello
+  {
+    static void Main()
+    {
+ 
+    Console.WriteLine("Hello World!");
+    // Console.WriteLine("Premi un tasto per terminare il programma");
+    // Console.ReadKey();
+    }
+  }
+}
