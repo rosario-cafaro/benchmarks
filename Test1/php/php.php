75489ccf23965307830c8a98a65b8faef5115e79
Rosario Cafaro <cafaro.rosario@gmail.com>
2021-10-02 20:21:55 +0200

    php read the file line by line

--- Test1/php/php.php
+++ Test1/php/php.php
@@ -30,11 +30,27 @@ function quickSort(&$arr, $leftIndex, $rightIndex)
         quickSort($arr, $index, $rightIndex);
 }
 
-try {
-    $integers = explode("\n", file_get_contents("../../data/10k_integers.txt"));
-} catch (Exception $e) {
-    echo $e->getMessage();
+// All file in memory
+// try {
+//     $integers = explode("\n", file_get_contents("../../data/10k_integers.txt"));
+// } catch (Exception $e) {
+//     echo $e->getMessage();
+// }
+
+
+$file = new SplFileObject("../../data/10k_integers.txt");
+
+// Loop until we reach the end of the file.
+$i = 0;
+while (!$file->eof()) {
+    // Echo one line from the file.
+    // echo $file->fgets();
+    $integers[$i++] = $file->fgets();
 }
+
+// Unset the file to call __destruct(), closing the file handle.
+$file = null;
+
 quickSort($integers, 0, count($integers) - 1);
 
 echo "First element: " . $integers[0] . "\nLast element: " . $integers[10000 - 1] . "\n";
